f5789379fa8ce0973142ada1079bd01b2eb80644
gky360 <gky360@gmail.com>
2018-11-07 15:03:04 +0900

    refactoring

--- application/params.h
+++ application/params.h
@@ -1,9 +1,6 @@
 #define INF 					999999999
 #define ITERATION 				3
 #define REQ_NUM 				100
-#define NODE_NUM 				11
-// directional links (NODE_NUM,LINK_NUM) = (11, 28), (5, 12), (14,44), (11, 52), (14, 46), (25, 84)
-#define LINK_NUM 				52
 #define CAPASITY 				400
 #define START_LOAD 				350
 #define HOLDING_TIME 			10
@@ -13,3 +10,7 @@
 #define DEFRAG_INTERVAL 		0.2
 #define MAX_STEP 				3
 #define MAX_HOP_NUM 			99999
+
+// (NODE_NUM,LINK_NUM) = (11, 28), (5, 12), (14,44), (11, 52), (14, 46), (25, 84)
+#define NODE_NUM 				11
+#define LINK_NUM 				52
