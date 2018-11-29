80faf1f1ad19fb1c5f7ec390b610e27a68e9d9e3
adshidtadka <nextage900fx@gmail.com>
2018-11-29 12:56:11 +0900

    arise error

--- application/params.h
+++ application/params.h
@@ -1,6 +1,6 @@
 #define INF 					999999999
-#define ITERATION 				5
-#define REQ_NUM 				10000
+#define ITERATION 				1
+#define REQ_NUM 				100
 #define CAPASITY 				400
 #define LOAD_START 				50
 #define LOAD_GAIN 				10
@@ -11,11 +11,11 @@
 #define DEFRAG_TOTAL_TIME_MAX 	100
 #define DEFRAG_INTERVAL 		0.2
 #define MAX_STEP 				3
-#define MAX_HOP_NUM 			99999
+#define MAX_HOP_NUM 			3
 // (NODE_NUM,LINK_NUM) = (11, 28), (5, 12), (14,44), (11, 52), (14, 46), (25, 84)
-#define NODE_NUM 				11
-#define LINK_NUM 				28
+#define NODE_NUM 				5
+#define LINK_NUM 				12
 #define SEED_1					1448601515
 #define SEED_2					125
-#define ALGO_START				0
+#define ALGO_START				2
 #define ALGO_LOOP				2
