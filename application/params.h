121d8453c6423112bb9db5999decbeac2d5eb534
adshidtadka <nextage900fx@gmail.com>
2019-02-03 14:01:01 +0900

    use const

--- application/params.h
+++ application/params.h
@@ -1,21 +1,21 @@
-#define INF 					999999999
-#define ITERATION 				3
-#define REQ_NUM 				10000
-#define CAPASITY 				400
-#define LOAD_START 				50
-#define LOAD_GAIN 				10
-#define LOAD_REPEAT_NUM			10
-#define HOLDING_TIME 			10
-#define REQ_SIZE_MAX 			16
-#define PROCESSING_TIME 		0.1
-#define DEFRAG_TOTAL_TIME_MAX 	100
-#define DEFRAG_INTERVAL 		0.2
-#define MAX_STEP 				3
-#define ADDITIONAL_HOP 			0
+const int INF                   = 999999999;
+const int ITERATION             = 3;
+const int REQ_NUM 				= 10000;
+const int CAPASITY 				= 400;
+const int LOAD_START 			= 50;
+const int LOAD_GAIN 			= 10;
+const int LOAD_REPEAT_NUM		= 10;
+const int HOLDING_TIME 			= 10;
+const int REQ_SIZE_MAX 			= 16;
+const double PROCESSING_TIME 	= 0.1;
+const int DEFRAG_TOTAL_TIME_MAX = 100;
+const double DEFRAG_INTERVAL 	= 0.2;
+const int MAX_STEP 				= 3;
+const int ADDITIONAL_HOP 		= 0;
 // (NODE_NUM,LINK_NUM) = (11, 28), (5, 12), (14,44), (11, 52), (14, 46), (25, 84)
-#define NODE_NUM 				11
-#define LINK_NUM 				28
-#define SEED_1					1448601515
-#define SEED_2					125
-#define ALGO_START				0
-#define ALGO_FINISH				2
+const int NODE_NUM 				= 11;
+const int LINK_NUM 				= 28;
+const int SEED_1				= 1448601515;
+const int SEED_2				= 125;
+const int ALGO_START			= 0;
+const int ALGO_FINISH			= 2;
